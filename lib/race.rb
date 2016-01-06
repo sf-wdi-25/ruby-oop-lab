360a08ac4b3cb432385c7b62c15f84a74252a217
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-06 11:26:10 -0800

    Completed lab

--- lib/race.rb
+++ lib/race.rb
@@ -4,4 +4,20 @@ class Race
 
   # write Race class code here
 
+  attr_accessor :cars
+
+  def initialize
+		@cars = [@first = Car.new, @second = Car.new]
+		@first.speed = rand(100)
+		@second.speed = rand(100)
+	end
+
+	def winner
+		@first.speed > @second.speed ? @first : @second
+	end
+
+	def loser
+		winner == @first ? @second : @first
+	end
+
 end
