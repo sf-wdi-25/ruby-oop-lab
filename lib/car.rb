aafb14d3488de7f85448c561da5c722fc223124d
kehontas <kehontas@gmail.com>
2016-01-06 13:40:17 -0800

    sorry only one completed commit

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,22 @@
 class Car
+	@@count = 0
+	attr_accessor :speed
 
+
+	def initialize
+		@speed = 0
+		@@count += 1
+	end
+
+
+	def accelerate(change)
+		@speed += change
+	end
+
+
+	def self.count
+		@@count 
+	end 
   # write Car class code here
 
 end
--- lib/race.rb
+++ lib/race.rb
@@ -1,7 +1,19 @@
 require_relative "car"
-
 class Race
-
-  # write Race class code here
-
+	def initialize
+		@car1 = Car.new
+		@car1.speed += rand(10)
+		@car2= Car.new
+		@car2.speed += rand(10)
+	end
+ 	def cars
+ 		[@car1, @car2]	
+ 	end
+ 	def winner
+ 		@car1.speed > @car2.speed ? @car1 : @car2
+ 	end
+ 	def loser
+ 	 	@car2.speed > @car1.speed ? @car1 : @car2
+ 	end
 end
+
