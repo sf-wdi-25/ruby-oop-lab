492120b50aed80c400eb237eb93248b00e846de4
Daniel Lwo <danielxji6@gmail.com>
2016-01-06 11:15:37 -0800

    Two cars racing new

--- lib/race.rb
+++ lib/race.rb
@@ -1,7 +1,20 @@
 require_relative "car"
 
 class Race
+  attr_accessor :cars
 
-  # write Race class code here
+  def initialize(color1 = 'Blue', color2 = 'Red')
+    @cars = [Car.new(color1), Car.new(color2)]
+    @cars[0].accelerate(rand(100))
+    @cars[1].accelerate(rand(100))
+  end
+
+  def winner
+    @cars[0].speed > @cars[1].speed ? @cars[0] : @cars[1]
+  end
+
+  def loser
+    @cars[0].speed < @cars[1].speed ? @cars[0] : @cars[1]
+  end
 
 end
